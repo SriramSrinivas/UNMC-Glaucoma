d875c9afe510c9e3cb5be64c424671c38c77fd1f
Abdullahi Mahamed <abdullahimahamed42@gmail.com>
2017-10-04 11:40:02 -0500

    added Regex class to check subject id

--- glacInit/Regex.swift
+++ glacInit/Regex.swift
@@ -5,5 +5,25 @@
 //  Created by Abdullahi Mahamed on 9/27/17.
 //  Copyright © 2017 Parshav Chauhan. All rights reserved.
 //
-
 import Foundation
+
+class Regex {
+    var internalExpression: NSRegularExpression?
+    var pattern: String
+    
+    init(_ pattern: String) {
+        self.pattern = pattern
+        
+        do {
+            self.internalExpression = try NSRegularExpression(pattern: pattern, options: NSRegularExpression.Options.caseInsensitive)
+        } catch {
+            print("Error regex bad")
+        }
+
+    }
+    
+    func test(input: String) -> Bool {
+        let matches = self.internalExpression?.matches(in: input, options: [], range: NSMakeRange(0, input.characters.count))
+        return matches!.count > 0
+    }
+}
