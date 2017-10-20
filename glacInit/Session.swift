52f7d5cf5b7632ca6ce982da715e66e34fedaca9
Abdullahi Mahamed <abdullahimahamed42@gmail.com>
2017-10-20 13:43:19 -0500

    storing session data

--- glacInit/Session.swift
+++ glacInit/Session.swift
@@ -7,3 +7,107 @@
 //
 
 import Foundation
+import UIKit
+import BoxContentSDK
+import SwiftMessages
+
+class Session {
+    var subjectId: String
+    var exportCount = 0
+    var distances =  [CGFloat]()
+    var savedFiles = [FileObject]()
+    //amount of times box tried to upload a file
+    var uploadAttempt = 0
+    var csvFilesUploadedCount = 0
+    var pngFilesUploadedCount = 0
+    
+    init(currentSubjectId: String) {
+        distances = [ -1, -0.8098, -0.6494, -0.5095, -0.3839, -0.2679, -0.158, -0.05, 0, 0.05, 0.158, 0.2679, 0.3839, 0.5095, 0.6494, 0.8098, 1]
+        subjectId = currentSubjectId
+        BOXContentClient.default().authenticate(completionBlock: { (user:BOXUser?, error:Error?) in
+            if (error == nil )
+            {
+                print((user?.login!)! as String)
+            }
+        })
+    }
+    
+    func saveGridData(mainView:UIView,customViewList:[CustomViewUpdate]){
+        exportCount = exportCount + 1
+        let width = mainView.frame.width
+        let height = mainView.frame.height
+        let halfWidth = width/2
+        let halfHeight = height/2
+        
+        var blurGrid = Matrix(rows:17,columns:17)
+        var greyGrid = Matrix(rows:17,columns:17)
+        var hiddenGrid = Matrix(rows: 17, columns: 17)
+        
+        for (row,i) in distances.enumerated() {
+            for (column,j) in distances.enumerated() {
+                let x = halfWidth + (halfWidth * j)
+                let y = halfHeight + (halfHeight * i)
+                
+                var greyValue = 0
+                var blurValue = 0
+                var hiddenValue = 0
+                for o in customViewList{
+                    if o.frame.contains(CGPoint(x: x, y: y)){
+                        
+                        if(o.blur.blurRadius > 0) {
+                            blurValue += o.viewValue
+                        }
+                        
+                        if(o.blur.backgroundColor == UIColor.black){
+                            greyValue += o.viewValue
+                        }
+                        
+                        if(o.isLinkedToImage){
+                            if(o.alphaValue == 0) {
+                                hiddenValue = hiddenValue + 10
+                            }
+                        }
+                        
+                    }
+                }
+                
+                blurGrid[row,column] = blurValue + blurGrid[row,column]
+                greyGrid[row,column] = greyValue + greyGrid[row,column]
+                hiddenGrid[row,column] = hiddenValue + hiddenGrid[row,column]
+            }
+        }
+        
+        let screenShotFile = FileObject(name: "\(subjectId)_screenshot_\(self.getTodayString())_\(exportCount)", type: FileType.PNG)
+        let blurPointsFile = FileObject(name:"\(subjectId)_blurPoints_\(getTodayString())_\(exportCount)",type:FileType.CSV)
+        let greyPointsFile = FileObject(name:"\(subjectId)_greyPoints_\(getTodayString())_\(exportCount)",type:FileType.CSV)
+        let hiddenPointsFile = FileObject(name:"\(subjectId)_hiddenPoints_\(getTodayString())_\(exportCount)",type:FileType.CSV)
+        
+        
+        screenShotFile.savePNG(view: mainView)
+        blurPointsFile.saveCSV(grid:blurGrid)
+        greyPointsFile.saveCSV(grid:greyGrid)
+        hiddenPointsFile.saveCSV(grid:hiddenGrid)
+        
+        savedFiles = [screenShotFile,blurPointsFile,greyPointsFile,hiddenPointsFile]
+    }
+    
+    private func getTodayString() -> String{
+        
+        let date = Date()
+        let calender = Calendar.current
+        let components = calender.dateComponents([.year,.month,.day,.hour,.minute,.second], from: date)
+        
+        let year = components.year
+        let month = components.month
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        let second = components.second
+        
+        let today_string = String(year!) + "-" + String(format: "%02d",month!) + "-" + String(format: "%02d",day!) + " " + String(format: "%02d",hour!)  + "-" + String(format: "%02d",minute!) + "-" +  String(format: "%02d",second!)
+        
+        return today_string
+        
+    }
+    
+}
