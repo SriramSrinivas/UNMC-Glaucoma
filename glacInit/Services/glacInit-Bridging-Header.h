4fa273830b5c8fb7b918b2cc8590542d8328b0ef
Lyle Reinholz <lreinholz@unomaha.edu>
2019-09-27 08:16:31 -0500

    needs this header file

---
+++ glacInit/Services/glacInit-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  glacInit-Bridging-Header.h
+//  glacInit
+//
+//  Created by Lyle Reinholz on 9/27/19.
+//  Copyright © 2019 Parshav Chauhan. All rights reserved.
+//
+
+#import "FileStuff.h"
